2cc416f167df3d94f95da30706c5d59431d90238
jvvier16 <javie.rojasc@duouc.cl>
2024-05-29 10:46:37 -0400

    estructura menu

---
+++ menugit.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+
+while True:
+    os.system('cls')
+    print("hola bienvenido ")
+    print("menu")
+    print("""1. inicio de sesion 
+             2. registrar usario
+             3. eliminar usario 
+             4. salir""")
+    opc = int(input("que opcion tomara"))
+    if opc ==1 :
+        pass
+    elif opc == 2:
+        pass
+    elif opc == 3:
+        pass
+    else:
+        print("saliste")
+        time.sleep(3)
+        break
